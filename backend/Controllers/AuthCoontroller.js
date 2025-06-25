19ec3cb930273e2aa7df10869cd05ef9752fe193
Shraddharai412 <167329332+Shraddharai412@users.noreply.github.com>
2025-06-25 12:41:30 +0530

    Update AuthCoontroller.js

--- backend/Controllers/AuthCoontroller.js
+++ backend/Controllers/AuthCoontroller.js
@@ -28,8 +28,8 @@ exports.signUp = async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:'lax',
-            secure:false
+            sameSite:'None',
+            secure:true
         })
         return res.status(201).json(user)
 
@@ -67,8 +67,8 @@ exports. Login = async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:'lax',
-            secure:false
+            sameSite:'None',
+            secure:true
         })
         return res.status(200).json(user)
 
